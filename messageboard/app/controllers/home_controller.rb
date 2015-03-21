1ca5c9bfee49067d451bfcff31f421ca2f24cb92
Daniel  Shafer <adamsmith@danielshafer.com>
2015-03-21 19:00:37 +0000

    added Devise for authentication

---
+++ messageboard/app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def index
+    #Get data from the database
+    #rearrange/process that data
+    #make additional database calls
+    #@Users = User.all
+  end
+end
